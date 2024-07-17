f61881b388e49ca395136599392868bfc26a524d
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-07-17 12:30:59 +0200

    add `conditional-statement` parsing

--- parser/src/parser/statement.rs
+++ parser/src/parser/statement.rs
@@ -7,6 +7,15 @@ use std::iter::Peekable;
 #[cfg_attr(test, derive(PartialEq))]
 pub enum Statement {
     Expression(Expression),
+    Conditional {
+        condition: Expression,
+        body: Box<Statement>,
+        otherwise: Option<Box<Statement>>,
+    },
+    Switch {
+        condition: Expression,
+        statement: Box<Statement>,
+    },
 }
 
 impl Parse for Statement {
@@ -20,6 +29,9 @@ impl Parse for Statement {
         let peek = tokens.peek().ok_or(ParserError::UnexpectedEndOfInput)?;
 
         let out = match peek {
+            Token::Keyword(Keyword::If) | Token::Keyword(Keyword::Switch) => {
+                Statement::conditional_statement(tokens, context)?
+            }
             _ => Statement::expression_statement(tokens, context)?,
         };
 
@@ -39,7 +51,54 @@ impl Statement {
                 tokens.next();
                 Statement::Expression(Expression::Empty)
             }
-            _ => Expression::parse(tokens, context).map(Statement::Expression)?,
+            _ => {
+                let statement = Expression::parse(tokens, context).map(Statement::Expression)?;
+                Self::expect_token(tokens, Token::SemiColon)?;
+                statement
+            }
+        };
+
+        Ok(out)
+    }
+
+    fn conditional_statement(
+        tokens: &mut Peekable<impl Iterator<Item = Token>>,
+        context: &Context,
+    ) -> Result<Self, ParserError> {
+        let token = tokens.next().ok_or(ParserError::UnexpectedEndOfInput)?;
+
+        let out = match token {
+            Token::Keyword(Keyword::If) => {
+                Self::expect_token(tokens, Token::OpenParenthesis)?;
+                let condition = Expression::parse(tokens, context)?;
+                Self::expect_token(tokens, Token::CloseParenthesis)?;
+                let body = Box::new(Statement::parse(tokens, context)?);
+                let otherwise = match tokens.peek() {
+                    Some(Token::Keyword(Keyword::Else)) => {
+                        tokens.next();
+                        Some(Box::new(Statement::parse(tokens, context)?))
+                    }
+                    _ => None,
+                };
+
+                Statement::Conditional {
+                    condition,
+                    body,
+                    otherwise,
+                }
+            }
+            Token::Keyword(Keyword::Switch) => {
+                Self::expect_token(tokens, Token::OpenParenthesis)?;
+                let condition = Expression::parse(tokens, context)?;
+                Self::expect_token(tokens, Token::CloseParenthesis)?;
+                let statement = Box::new(Statement::parse(tokens, context)?);
+
+                Statement::Switch {
+                    condition,
+                    statement,
+                }
+            }
+            _ => unreachable!(),
         };
 
         Ok(out)
@@ -70,6 +129,139 @@ mod tests {
         let context = Context::default();
         let result =
             Statement::expression_statement(&mut input.into_iter().peekable(), &context).unwrap();
+        assert_eq!(result, expected);
+    }
+
+    #[rstest]
+    #[case::if_statement(
+        vec![
+            Token::Keyword(Keyword::If),
+            Token::OpenParenthesis,
+            Token::Word("a".to_string()),
+            Token::CloseParenthesis,
+            Token::Word("b".to_string()),
+            Token::SemiColon,
+        ],
+        Statement::Conditional {
+            condition: Expression::Identifier("a".to_string()),
+            body: Box::new(Statement::Expression(Expression::Identifier("b".to_string()))),
+            otherwise: None,
+        }
+    )]
+    #[case::if_else_statement(
+        vec![
+            Token::Keyword(Keyword::If),
+            Token::OpenParenthesis,
+            Token::Word("a".to_string()),
+            Token::CloseParenthesis,
+            Token::Word("b".to_string()),
+            Token::SemiColon,
+            Token::Keyword(Keyword::Else),
+            Token::Word("c".to_string()),
+            Token::SemiColon,
+        ],
+        Statement::Conditional {
+            condition: Expression::Identifier("a".to_string()),
+            body: Box::new(Statement::Expression(Expression::Identifier("b".to_string()))),
+            otherwise: Some(Box::new(Statement::Expression(Expression::Identifier("c".to_string())))),
+        }
+    )]
+    #[case::if_compound(
+        vec![
+            Token::Keyword(Keyword::If),
+            Token::OpenParenthesis,
+            Token::Word("a".to_string()),
+            Token::CloseParenthesis,
+            Token::OpenBrace,
+            Token::Word("b".to_string()),
+            Token::SemiColon,
+            Token::CloseBrace,
+        ],
+        Statement::Conditional {
+            condition: Expression::Identifier("a".to_string()),
+            body: Box::new(Statement::Expression(Expression::Identifier("b".to_string()))),
+            otherwise: None,
+        }
+    )]
+    #[case::if_else_compound(
+        vec![
+            Token::Keyword(Keyword::If),
+            Token::OpenParenthesis,
+            Token::Word("a".to_string()),
+            Token::CloseParenthesis,
+            Token::OpenBrace,
+            Token::Word("b".to_string()),
+            Token::SemiColon,
+            Token::CloseBrace,
+            Token::Keyword(Keyword::Else),
+            Token::OpenBrace,
+            Token::Word("c".to_string()),
+            Token::SemiColon,
+            Token::CloseBrace,
+        ],
+        Statement::Conditional {
+            condition: Expression::Identifier("a".to_string()),
+            body: Box::new(Statement::Expression(Expression::Identifier("b".to_string()))),
+            otherwise: Some(Box::new(Statement::Expression(Expression::Identifier("c".to_string())))),
+        }
+    )]
+    #[case::switch(
+        vec![
+            Token::Keyword(Keyword::Switch),
+            Token::OpenParenthesis,
+            Token::Word("a".to_string()),
+            Token::CloseParenthesis,
+            Token::OpenBrace,
+            Token::CloseBrace,
+        ],
+        Statement::Switch {
+            condition: Expression::Identifier("a".to_string()),
+            statement: Box::new(Statement::Expression(Expression::Empty)),
+        }
+    )]
+    #[case::switch_default(
+        vec![
+            Token::Keyword(Keyword::Switch),
+            Token::OpenParenthesis,
+            Token::Word("a".to_string()),
+            Token::CloseParenthesis,
+            Token::OpenBrace,
+            Token::Keyword(Keyword::Default),
+            Token::Colon,
+            Token::CloseBrace,
+        ],
+        Statement::Switch {
+            condition: Expression::Identifier("a".to_string()),
+            statement: Box::new(Statement::Expression(Expression::Empty)),
+        }
+    )]
+    #[case::switch_cased(
+        vec![
+            Token::Keyword(Keyword::Switch),
+            Token::OpenParenthesis,
+            Token::Word("a".to_string()),
+            Token::CloseParenthesis,
+            Token::OpenBrace,
+            Token::Keyword(Keyword::Case),
+            Token::Word("1".to_string()),
+            Token::Colon,
+            Token::Keyword(Keyword::Break),
+            Token::SemiColon,
+            Token::Keyword(Keyword::Default),
+            Token::Colon,
+            Token::Keyword(Keyword::Break),
+            Token::SemiColon,
+            Token::CloseBrace,
+        ],
+        Statement::Switch {
+            condition: Expression::Identifier("a".to_string()),
+            statement: Box::new(Statement::Expression(Expression::Empty)),
+        }
+    )]
+    fn test_conditional_statement(#[case] input: Vec<Token>, #[case] expected: Statement) {
+        let context = Context::default();
+        let result =
+            Statement::conditional_statement(&mut input.into_iter().peekable(), &context).unwrap();
         assert_eq!(result, expected);
     }
 }
